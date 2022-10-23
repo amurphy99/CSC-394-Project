31b4216f7d54ede7f3156ec6c3a9bf004186c380
Joseph Reyes <jreyes1919@gmail.com>
2022-10-23 14:36:10 -0500

    added homepage

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -57,6 +57,9 @@ def create_app(test_config=None):
     from . import user_page
     app.add_url_rule('/user/<userID>',           methods=('GET', 'POST'), view_func=user_page.user_page)
 
+    from . import home_page
+    app.add_url_rule('/home_page/<userID>', methods=('GET', 'POST'), view_func=home_page.home_page)
+
     # a simple page that says hello
     @app.route('/test3')
     def test3():
