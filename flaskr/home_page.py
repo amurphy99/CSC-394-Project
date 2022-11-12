2e7c15846a64430381fbf2b10add23119d48a9c7
Joseph Reyes <jreyes1919@gmail.com>
2022-11-12 12:58:50 -0600

    started genres filter

--- flaskr/home_page.py
+++ flaskr/home_page.py
@@ -16,11 +16,85 @@ def home_page():
     BASE_URL    = "http://image.tmdb.org/t/p/"
     POSTER_SIZE = "w500"
 
-    #Search url
-    # base        = "https://api.themoviedb.org/3/search/movie?api_key="
-    # api_key     = "f059b4ab8738e8777362529e74ffb62a"
-    # search_url  = base + api_key
-
+    # Genres
+    genres = [
+      {
+         "id":28,
+         "name":"Action"
+      },
+      {
+         "id":12,
+         "name":"Adventure"
+      },
+      {
+         "id":16,
+         "name":"Animation"
+      },
+      {
+         "id":35,
+         "name":"Comedy"
+      },
+      {
+         "id":80,
+         "name":"Crime"
+      },
+      {
+         "id":99,
+         "name":"Documentary"
+      },
+      {
+         "id":18,
+         "name":"Drama"
+      },
+      {
+         "id":10751,
+         "name":"Family"
+      },
+      {
+         "id":14,
+         "name":"Fantasy"
+      },
+      {
+         "id":36,
+         "name":"History"
+      },
+      {
+         "id":27,
+         "name":"Horror"
+      },
+      {
+         "id":10402,
+         "name":"Music"
+      },
+      {
+         "id":9648,
+         "name":"Mystery"
+      },
+      {
+         "id":10749,
+         "name":"Romance"
+      },
+      {
+         "id":878,
+         "name":"Science Fiction"
+      },
+      {
+         "id":10770,
+         "name":"TV Movie"
+      },
+      {
+         "id":53,
+         "name":"Thriller"
+      },
+      {
+         "id":10752,
+         "name":"War"
+      },
+      {
+         "id":37,
+         "name":"Western"
+      }
+   ]
 
     # api call to get trending movies
     if request.method == "POST":
@@ -51,7 +125,7 @@ def home_page():
                         ["Benas",   7]  ]
 
 
-    print(movieDisplay)
+    # print(movieDisplay)
 
     
     # display page
